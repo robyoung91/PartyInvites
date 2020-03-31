7628fdd15f26d50c0884df471638493aa83ab23b
robyoung91 <robertmatthewyoung@gmail.com>
2020-03-31 11:51:18 +0100

    Finished adding functionality, validation and bootstrap styling.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -23,9 +23,15 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            Repository.AddResponse(guestResponse);
-            // TODO: store response from guest
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else
+            {
+                // there is a validation error
+                return View();
+            }
         }
 
         [HttpGet]
